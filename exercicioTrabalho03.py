b494536d41281743c8478aced0f256be74380713
Bruno Vigel <brunorosav@gmail.com>
2023-01-19 14:48:57 -0300

    comentários para os ex 2 e 3

--- exercicioTrabalho03.py
+++ exercicioTrabalho03.py
@@ -2,19 +2,19 @@ def metragemLimpeza():
     print('----------------- Menu 1 de 3 - Metragem Limpeza ------------------')
     while True:
         try:
-            metragemCasa = int(input('Entre com a metragem da casa: '))
-            if metragemCasa >= 30 and metragemCasa < 300:
+            metragemCasa = int(input('Entre com a metragem da casa: ')) # Recebe o valor do tamanho da casa
+            if metragemCasa >= 30 and metragemCasa < 300: # Verifica o tamanho recebido
                 print('É necessário contratar 1 pessoa')
-                return 60 + 0.3 * metragemCasa
+                return 60 + 0.3 * metragemCasa  # Retorna ********** TERMINAR
             elif metragemCasa >= 300 and metragemCasa < 700:
                 print('É necessário contratar 2 pessoas')
                 return 12 + 0.5 * metragemCasa
             else:
                 print('!! Não aceitamos casas com metragem menor que 30m² ou maior que 700m²')
-                continue
+                continue # Caso o valor sejá inválido retorna ao início do laço
         except ValueError:
             print('Valor inválido, apenas valores númericos inteiros são aceitos.')
-            continue
+            continue # Caso o valor sejá inválido retorna ao início do laço
 
 def tipoLimpeza():
     print('------------------ Menu 2 de 3 - Tipo de Limpeza ------------------')
@@ -22,14 +22,14 @@ def tipoLimpeza():
         limpezaEscolhida = input('Entre com o tipo de limpeza: \n' + 
                                 'B - Básica - Indicada para sujeiras semanais ou quizenais \n' + 
                                 'C - Completa - (30% a mais) - Indicada para sujeiras antigas e/ou não rotineiras. \n' +
-                                '>>').upper()
-        if limpezaEscolhida == 'B':
-            return 1.00
+                                '>>').upper() # Recebe o valor do tipo de limpeza e o deixa em caixa alta para deixar o tratamento de erros mais fácil
+        if limpezaEscolhida == 'B': # Checa o tipo escolhido
+            return 1.00 # Retorna o multiplicador
         elif limpezaEscolhida == 'C':
             return 1.30
         else:
             print('!!!!!!! Opção inválida !!!!!!!')
-            continue
+            continue # Caso o valor sejá inválido retorna ao início do laço
 
 def adicionalLimpeza():
     print('--------------- Menu 2 de 3 - Adicional de Limpeza ----------------')
@@ -40,12 +40,12 @@ def adicionalLimpeza():
                 '1 - Passar 10 peças de roupas - R$ 10,00 \n' +
                 '2 - Limpeza de 1 Forno/Micro-ondas - R$ 12,00 \n' +
                 '3 - Limpeza de 1 Geladeira/Freezer - R$ 20,00 \n' +
-                '>>')
-        if adicional == '0':
-            return acumulador
+                '>>') # Recebe o valor referente ao tipo de adicional
+        if adicional == '0': # verifica o valor recebido
+            return acumulador # retorna o valor do acumulador
         elif adicional == '1':
-            acumulador += 10
-            continue
+            acumulador += 10 # recebe o acumulador + 10
+            continue # retorna ao início do laço
         elif adicional == '2':
             acumulador += 12
             continue
@@ -54,7 +54,7 @@ def adicionalLimpeza():
             continue
         else:
             print('!!!!!!! Opção inválida !!!!!!!')
-            continue
+            continue # Caso o valor sejá inválido retorna ao início do laço
 
         
 print('Bem vindo ao Programa de Serviços de Limpeza do Bruno da Rosa Vigel')
@@ -62,7 +62,7 @@ print('*******************************************************************')
 valorMetragem = metragemLimpeza()
 tipo = tipoLimpeza()
 totalAdicional = adicionalLimpeza()
-valorTotal = valorMetragem * tipo + totalAdicional
+valorTotal = valorMetragem * tipo + totalAdicional # calcula o valor total
 
 print('TOTAL: R$ {:.2f} (metragem: {:.2f} * tipo: {:.2f} + adicional: {:.2f}'.format(valorTotal, valorMetragem, tipo, totalAdicional))
 
